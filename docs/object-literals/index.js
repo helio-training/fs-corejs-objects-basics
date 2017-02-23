806a6bee8ecc93bfbe0f7be64bb779586a414e44
Thor Anderson <ta.anderson@gmail.com>
2017-02-23 12:14:37 -0700

    Cleaned up white space

--- docs/object-literals/index.js
+++ docs/object-literals/index.js
@@ -4,41 +4,24 @@ export const FACTIONS = {
   Decepticons: 'decepticons'
 };
 
-
-
-
 // define your Optimus Prime object below
 export const OptimusPrime = {
   name: 'Optimus Prime',
   faction: FACTIONS.Autobots,
   power: 1000,
 
-  greet() {
+  greet() { // don't forget to include the greet() function!
     return `Hi, I'm ${this.name}`;
   }
 };
 
-
-
-// don't forget to include the greet() function!
-
-
-
-
-
 // define your Megatron object below
-
 export const Megatron = {
   name: 'Megatron',
   faction: FACTIONS.Decepticons,
   power: 1000,
 
-  greet() {
+  greet() { // Megatron also has a greet method
     return `Hi, I'm ${this.name}`;
   }
 };
-
-
-
-
-// Megatron also has a greet method
